cda487fdc52434dafd9d261e3ec1c3e7db17cce2
linusbayere <linusbayere@gmail.com>
2025-01-02 05:45:17 -0500

    fix: remove data_source for subnet

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -43,7 +43,7 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.eks_cluster_role.arn
 
   vpc_config {
-    subnet_ids = data.aws_subnets.eks.ids
+    subnet_ids = aws_subnet.public[*].id
   }
 
   depends_on = [
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -7,3 +7,8 @@ output "cluster_name" {
   description = "eks cluster name"
   value       = aws_eks_cluster.main.name
 }
+
+output "eks_subnet_ids" {
+  description = "List of subnet IDs for EKS cluster."
+  value       = aws_subnet.public[*].id
+}
